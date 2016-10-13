cd4bb8470a22aa8faa00b27c7f2e17085d206668
735579768@qq.com <735579768@qq.com>
2016-10-13 12:44:26 +0800

    修复多个实例上传时点击无效的bug

--- fileupload.php
+++ fileupload.php
@@ -1,6 +1,6 @@
 <?php
 // array(1) {
-//   ["webuploaderimg"]=>
+//   ["filelist"]=>
 //   array(5) {
 //     ["name"]=>
 //     string(9) "image.png"
@@ -17,18 +17,18 @@
 header('Content-type: application/json; charset=utf-8'); //json
 $action = @$_GET['action'];
 if ($action == 'img') {
-	echo '{"info":[{"id":"15686","uid":"1","srcname":"145852218011046.jpg","destname":"147253995925844.jpg","path":"https://www.zhaokeli.com/Public/Home/blog/images/touxiang.jpg","thumbpath":"https://www.zhaokeli.com/Public/Home/blog/images/touxiang.jpg","status":"1","from":"","extra":"","sha1":"ec121869dc3a0caf86da93eb8ef37e7ee88d56a7","create_time":"1472539959"},{"id":"15686","uid":"1","srcname":"145852218011046.jpg","destname":"147253995925844.jpg","path":"https://www.zhaokeli.com/Public/Home/blog/images/touxiang.jpg","thumbpath":"https://www.zhaokeli.com/Public/Home/blog/images/touxiang.jpg","status":"1","from":"","extra":"","sha1":"ec121869dc3a0caf86da93eb8ef37e7ee88d56a7","create_time":"1472539959"}],"status":1,"url":""}';
+	echo '{"data":[{"id":"15686","uid":"1","srcname":"145852218011046.jpg","destname":"147253995925844.jpg","path":"https://www.zhaokeli.com/Public/Home/blog/images/touxiang.jpg","thumbpath":"https://www.zhaokeli.com/Public/Home/blog/images/touxiang.jpg","status":"1","from":"","extra":"","sha1":"ec121869dc3a0caf86da93eb8ef37e7ee88d56a7","create_time":"1472539959"},{"id":"15686","uid":"1","srcname":"145852218011046.jpg","destname":"147253995925844.jpg","path":"https://www.zhaokeli.com/Public/Home/blog/images/touxiang.jpg","thumbpath":"https://www.zhaokeli.com/Public/Home/blog/images/touxiang.jpg","status":"1","from":"","extra":"","sha1":"ec121869dc3a0caf86da93eb8ef37e7ee88d56a7","create_time":"1472539959"}],"status":1,"url":""}';
 } else {
 	$redata = [
 		'id'     => 89,
 		'status' => 1,
 		'data'   => $_FILES,
 	];
-	$filename = $_FILES['webuploaderimg']['name'];
+	$filename = $_FILES['filelist']['name'];
 	$filename = explode('.', $filename);
 	$ext      = $filename[count($filename) - 1];
 	$filename = date('ymdhis') . '.' . $ext;
-	$bl       = move_uploaded_file($_FILES['webuploaderimg']['tmp_name'], './uploads/' . $filename);
+	$bl       = move_uploaded_file($_FILES['filelist']['tmp_name'], './uploads/' . $filename);
 	if (!$bl) {
 		$redata['status'] = 0;
 	}
